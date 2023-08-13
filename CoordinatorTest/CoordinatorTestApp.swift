f0133e2adf7ba20d666f589f142ee813c6f9e9e7
백상휘 <sanghwiback@gmail.com>
2023-08-13 10:34:30 +0900

    Initial Commit

---
+++ CoordinatorTest/CoordinatorTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoordinatorTestApp.swift
+//  CoordinatorTest
+//
+//  Created by 백상휘 on 2023/08/13.
+//
+
+import SwiftUI
+
+@main
+struct CoordinatorTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
